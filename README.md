b08b4d6d4f8a8fb434c67da34309d9a08161e33a
Manuel Engel <manuel-engel-97@gmx.de>
2021-01-24 17:18:06 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# You are great!
+
+## Project information
+
+Especially in those times, it is important to not completely sink into negative thoughts, which can be very hard some
+days.
+This small Python script displays positive phrases in a randcmized order. The displayed sentence changes every five
+minutes. Give it a try, let it run alongside your other applications, look at it sometimes. Of course, no success
+can be guaranteed, but it will most likely not cause any harm.
+
+## Technical details
+The script needs Python 3.6 or above to run.
+
+## Cooperation / Collaboration
+You can contribute to the project if you feel like it. In this case, you agree that your contributions will become
+part of the MIT license.
+Possible tasks:
+* Add support for other languages
+* Add command line params
---
+++ main.py
@@ -0,0 +1,45 @@
+# !/usr/bin/python3
+import secrets
+from time import sleep
+from os import name, system
+
+# Possible mantras which can be printed on-screen.
+# This list contains positive affirmations.
+messages_en = [
+    "You are great, because you are who you are.",
+    "You are a valuable human being.",
+    "Do not give up. Even if it seems to be the only way out.",
+    "When you expect it the least, the most miracles will happen.",
+    "You will never be truly alone.",
+    "You are beautiful.",
+    "When others insult you: They are the weak ones, not you.",
+    "Right now, there is somebody who believes in you.",
+    "Think about your achievements.",
+    "You are great!",
+    "Not overestimating yourself is good. But underestimation won't do you any good either.",
+    "Nothing did stop you completely. It will stay like this.",
+    "Even in the greatest darkness, there is some light.",
+    "Seek the society of people who appreciate you, avoid those of people who do not.",
+    "You also have skills and qualities.",
+    "You most likely accomplished something today. Even if it is just something tiny.",
+    "If you see this, you most likely mastered some challenges in the past.",
+    "You deserve to be appreciated. Do not let somebody tell you something else.",
+    "There is nothing like a 'normal' person. Be yourself, do not wear a theatre mask.",
+    "Seeking help from others is no sign of weakness.",
+    "Allow yourself to be happy and sad alike.",
+    "Emotions are part of your nature.",
+    "Everyone has bad days. But those days will eventually pass and good ones will come.",
+    "There is nobody exactly like you again. You are a unique treasure.",
+    "The sun will always return."
+]
+
+while True:
+    if name == "nt":
+        system("cls")
+
+    else:
+        system("clear")
+
+    randomIndex = secrets.randbelow(len(messages_en))
+    print(messages_en[randomIndex])
+    sleep(300)
