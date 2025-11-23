2382c09a84f70ef44107f0378219b194cc989317
Manuel Engel <manuel-engel-97@gmx.de>
2025-11-23 22:17:54 +0100

    Added another affirmation.

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ messages_en = [
     "It is sometimes very hard to be honest. But it will pay off in the end.",
     "Everyone is a star. We all just need to find our way to shine.",
     "You have potential! You will eventually find a way to use it.",
+    "What appears to be a disadvantage in one situation, can turn out to be a valuable gift in another one.",
 ]
 
 use_notify = len(argv) > 1 and argv[1] == "-n"
